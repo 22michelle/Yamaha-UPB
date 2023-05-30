9b188af7d843748b6da45a6dec31078c14c0b71f
dana <danacarrillo1984@gmail.com>
2023-05-30 18:47:01 -0500

    new styles

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import usersReducer from './usersReducer'
+import postsReducer from './postsReducer'
+
+const store = configureStore({
+    reducer: {
+        users: usersReducer,
+        posts: postsReducer,
+    },
+})
+
+export default store
