3f5e102aefc8a11abb6935e87c789d381790d504
Michelle <michellemejiaparra4@gmail.com>
2023-06-01 10:47:40 -0500

    Realiza cambios en archivos de estilos navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../components/home/Header";
 import Footer from "../components/home/Footer";
+import { Navbar } from "../components/common/Navbar";
 
 export const Home = () => {
   return (
     <div>
+      <Navbar/>
       <Header />
       <Footer/>
     </div>
