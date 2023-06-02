fa15e0b711e5c4996e85b21c9e27025511c97271
dana <danacarrillo1984@gmail.com>
2023-06-02 15:35:27 -0500

    slice auth

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,20 +1,44 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     user: null,
     token: null,
-    isLoged: false
-}
+    isLoged: false,
+};
 
 export const login = createAsyncThunk(
     "authSlice/login",
     async(user, { dispatch, rejectWithValue }) => {
         try {
-            const { data, token } = await axios.post(`/login`, user)
-            localStorage.setItem("user", JSON.stringify(data.data))
-            return data.data
+            const { data, token } = await axios.post(`/login`, user);
+            localStorage.setItem("user", JSON.stringify(data.data));
+            return data.data;
         } catch (error) {
-            return rejectWithValue(error.respo)
+            return rejectWithValue(error.response.data.message);
         }
     }
-)
+);
+
+export const verifyLogin = createAsyncThunk(
+    "authSlice/verifyLogin",
+    async(_, { dispatch, rejectWithValue }) => {
+        try {
+            const data = localStorage.getItem(user);
+            if (!data) {
+                return dispatch(logout());
+            }
+            return JSON.parse(data)
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    }
+);
+
+export const logout = createAsyncThunk("authSlice/logout", async(_, {}) => {
+    localStorage.removeItem("user");
+});
+
+
+const authSlice = createSlice({
+
+})
